2740b6dd1df2d7eda117562957c235132bf36e1e
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-11-24 00:50:50 +0000

    test(configure msw): configure msw

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
 export default {
+  setupFiles: ['<rootDir>./tests/config/jest/jest.polyfills.ts'],
+  setupFilesAfterEnv: ['<rootDir>./tests/config/jest/jest.setup.ts'],
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironmentOptions: {
+    customExportConditions: [''],
   },
   collectCoverageFrom: [
     '**/*.{ts,tsx}',
@@ -15,9 +20,18 @@ export default {
     '!**/index.ts',
     '!**/routes/**',
   ],
+
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/functions/(.*)$': '<rootDir>/src/functions/$1',
+    '^@/middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^@/constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@/store/(.*)$': '<rootDir>/src/store/$1',
+    '^@/services/(.*)$': '<rootDir>/src/services/$1',
+    '^@/providers/(.*)$': '<rootDir>/src/providers/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
   },
-  setupFilesAfterEnv: ['<rootDir>./tests/config/jest/jest.setup.ts'],
 }
--- tests/config/jest/jest.setup.ts
+++ tests/config/jest/jest.setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom'
+import { server } from '../mock-requests'
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
