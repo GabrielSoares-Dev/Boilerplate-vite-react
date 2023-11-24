18e4ebf3fc276cd47b701c6317babb6725b5bbfb
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2023-11-24 12:42:23 +0000

    fix(fix husky): fix

--- src/providers/app-provider/index.tsx
+++ src/providers/app-provider/index.tsx
@@ -1,7 +1,6 @@
-import type { AppProviderProps } from '@providers'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import type { AppProviderProps } from '@providers'
 const queryClient = new QueryClient()
 
 export function AppProvider({ children }: AppProviderProps) {
